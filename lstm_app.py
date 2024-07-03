89dcf4be62a2aa4e93cf1d97e6e1a8ec6a1d1810
anil-barwar89 <a_anil.barwar@roundcircle.tech>
2024-07-03 21:03:51 +0530

    Update lstm_app.py
    file added for all the categories with side bar

--- lstm_app.py
+++ lstm_app.py
@@ -101,28 +101,80 @@ secondary_model_paths = {
     'Business Intelligence and Analytics': 'LSTM_models/bi/L2_bi_model.h5',
     'Construction and Real Estate Development': 'LSTM_models/construction/L2_construction_model.h5',
     'Customer Relationship Management': 'LSTM_models/crm/L2_crm_model.h5',
-    'Education and Training': 'LSTM_models/education/L2_education_model.h5'
+    'Education and Training': 'LSTM_models/education/L2_education_model.h5',
+    "Finance and Accounting" : 'LSTM_models/finance/L2_finance_model.h5', 
+    "Fleet and Transportation Management" :'LSTM_models/transport/L2_transport_model.h5',
+    "Greeting_l1": 'LSTM_models/greeting/L2_greeting_model.h5',
+    "HR": 'LSTM_models/hr/L2_hr_model.h5',
+    "Healthcare and Medical":'LSTM_models/health/L2_health_model.h5',
+    "Hospitality and Travel": 'LSTM_models/travel/L2_travel_model.h5',
+    "IT and Communications": 'LSTM_models/it/L2_it_model.h5',
+    "Legal and Risk Management": 'LSTM_models/legal/L2_legal_model.h5',
+    "Manufacturing and Supply Chain": 'LSTM_models/scm/L2_scm_model.h5',
+    "Marketing and Sales": 'LSTM_models/marketing/L2_marketing_model.h5',
+    "Nonprofit and Social Services": 'LSTM_models/nonprofit/L2_nonprofit_model.h5',
+    "Project and Operations Management" : 'LSTM_models/pm/L2_pm_model.h5',
+    "Retail and Ecommerce": 'LSTM_models/retail/L2_retail_model.h5'
 }
+
 
 secondary_tokenizer_paths = {
     'Business Intelligence and Analytics': 'LSTM_models/bi/L2_bi_tokenizer.pkl',
     'Construction and Real Estate Development': 'LSTM_models/construction/L2_construction_tokenizer.pkl',
     'Customer Relationship Management': 'LSTM_models/crm/L2_crm_tokenizer.pkl',
-    'Education and Training': 'LSTM_models/education/L2_education_tokenizer.pkl'
+    'Education and Training': 'LSTM_models/education/L2_education_tokenizer.pkl',
+    "Finance and Accounting" : 'LSTM_models/finance/L2_finance_tokenizer.pkl', 
+    "Fleet and Transportation Management" :'LSTM_models/transport/L2_transport_tokenizer.pkl',
+    "Greeting_l1": 'LSTM_models/greeting/L2_greeting_tokenizer.pkl',
+    "HR": 'LSTM_models/hr/L2_hr_tokenizer.pkl',
+    "Healthcare and Medical":'LSTM_models/health/L2_health_tokenizer.pkl',
+    "Hospitality and Travel": 'LSTM_models/travel/L2_travel_tokenizer.pkl',
+    "IT and Communications": 'LSTM_models/it/L2_it_tokenizer.pkl',
+    "Legal and Risk Management": 'LSTM_models/legal/L2_legal_tokenizer.pkl',
+    "Manufacturing and Supply Chain": 'LSTM_models/scm/L2_scm_tokenizer.pkl',
+    "Marketing and Sales": 'LSTM_models/marketing/L2_marketing_tokenizer.pkl',
+    "Nonprofit and Social Services": 'LSTM_models/nonprofit/L2_nonprofit_tokenizer.pkl',
+    "Project and Operations Management" : 'LSTM_models/pm/L2_pm_tokenizer.pkl',
+    "Retail and Ecommerce": 'LSTM_models/retail/L2_retail_tokenizer.pkl'
 }
 
 secondary_label_paths = {
     'Business Intelligence and Analytics': 'LSTM_models/bi/L2_bi_dct.json',
     'Construction and Real Estate Development': 'LSTM_models/construction/L2_construction_dct.json',
     'Customer Relationship Management': 'LSTM_models/crm/L2_crm_dct.json',
-    'Education and Training': 'LSTM_models/education/L2_education_dct.json'
-}
+    'Education and Training': 'LSTM_models/education/L2_education_dct.json',
+    "Finance and Accounting" : 'LSTM_models/finance/L2_finance_dct.json', 
+    "Fleet and Transportation Management" :'LSTM_models/transport/L2_transport_dct.json',
+    "Greeting_l1": 'LSTM_models/greeting/L2_greeting_dct.json',
+    "HR": 'LSTM_models/hr/L2_hr_dct.json',
+    "Healthcare and Medical":'LSTM_models/health/L2_health_dct.json',
+    "Hospitality and Travel": 'LSTM_models/travel/L2_travel_dct.json',
+    "IT and Communications": 'LSTM_models/it/L2_it_dct.json',
+    "Legal and Risk Management": 'LSTM_models/legal/L2_legal_dct.json',
+    "Manufacturing and Supply Chain": 'LSTM_models/scm/L2_scm_dct.json',
+    "Marketing and Sales": 'LSTM_models/marketing/L2_marketing_dct.json',
+    "Nonprofit and Social Services": 'LSTM_models/nonprofit/L2_nonprofit_dct.json',
+    "Project and Operations Management" : 'LSTM_models/pm/L2_pm_dct.json',
+    "Retail and Ecommerce": 'LSTM_models/retail/L2_retail_dct.json'
 
 # Initialize the classifier
 classifier = HierarchicalTextClassifier(main_model_path, main_tokenizer_path, main_label_path,
                                         secondary_model_paths, secondary_tokenizer_paths, secondary_label_paths)
 
 st.title('Hierarchical Text Classifier')
+
+# Sidebar menu
+st.sidebar.title('Catgories')
+st.sidebar.markdown('write a question from any of the below category to classify the text:')
+
+categories = list(secondary_model_paths.keys())
+selected_category = st.sidebar.radio('Categories', categories)
+
+if st.sidebar.button("About Us"):
+    st.sidebar.markdown("""
+    This is a Streamlit app demonstrating hierarchical text classification using deep learning models.
+    """)
+
 st.write("Enter text for classification:")
 
 text_input = st.text_area("Input text")
